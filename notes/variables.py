247ee16dc65269c4ee5394f0a1e2bdc220993d4f
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-01-21 13:43:31 -0700

    Variables notes

--- notes/variables.py
+++ notes/variables.py
@@ -1,3 +1,16 @@
 name = "Lupus"
 
+money = -1000000
+
+pi = 3.14
+
+wolf = input("What is your favorite wolf species: ")
+time = "2130"
+
+countdown = 3
+
 print(name)
+print("Sidus Lupus means Star Wolf")
+print("Foxes are nice", "Wolves are better")
+
+print(wolf, "is a really cool species!")
