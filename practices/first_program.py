ee1ff043e1ec9077f9e9ac1a81335c8ce943990a
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-01-22 13:22:51 -0700

    First python program and variable practice completed

---
+++ practices/first_program.py
@@ -0,0 +1,13 @@
+# CW First Program
+
+name = input("What is your name: ")
+
+print(name, "huh? Nice to meet you!")
+
+smash_brothers_main = input("What character do you main in Super Smash Brothers Ultimate: ")
+
+print("I can't belive you would main", smash_brothers_main, "you casual.")
+
+destiny_2_main_class = input("What class do you main in Destiny 2: ")
+
+print("Wow! I main", destiny_2_main_class, "too!")
---
+++ practices/variables_practice.py
@@ -0,0 +1,14 @@
+# CW Variables Practice
+
+name = "Lupus"
+number = 9
+big_number = 543
+breakfast = "pancakes"
+color = "blue"
+school_name = "UCAS"
+current_year = 2026
+eye_color = "brown"
+age = 14
+favorite_subject = "music"
+
+print(name, number, big_number, breakfast, color, school_name, current_year, eye_color, age, favorite_subject)
