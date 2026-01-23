e0b03c07e5a3c12b8cc46380be472af063c52e68
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-01-23 13:42:31 -0700

    Finished inters floats notes

---
+++ notes/integers_floats.py
@@ -0,0 +1,15 @@
+#CW Integers and Floats
+
+"""price = float(input("Enter original price: $"))
+
+tax_rate = float(input("Enter tax percent: %"))/100
+
+total_price = price*(1+ tax_rate)
+
+print(round(total_price, 2))"""
+
+apples = int(input("How many apples are there: "))
+
+friends = int(input("How many friends are there: "))
+
+print("Each friend gets", apples//friends, "apples and I have", apples%friends, "to myself")
