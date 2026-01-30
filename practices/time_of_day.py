b407e5832d683d1e8c270c3f12eb32c589048065
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-01-30 13:20:41 -0700

    added night time response to time of day

--- practices/time_of_day.py
+++ practices/time_of_day.py
@@ -11,16 +11,19 @@ def displayTimeEnglish ():
         print("Good morning user")
     elif current_time >= 1200 and current_time < 1800:
         print("Good afternoon user")
-    else:
+    elif current_time >= 1800:
         print("Good evening user")
-
+    else:
+        print("Go to bed user")
 def displayTimeGerman ():
     if current_time >= 600 and current_time < 1200:
         print("Guten Morgen Benuzter")
     elif current_time >= 1200 and current_time < 1800:
         print("Guten Tag Benuzter")
-    else:
+    elif current_time >= 1800:
         print("Guten Abend Benuzter")
+    else:
+        print("Geh ins Bett, Benutzer")
 
 if display_language == "english":
     displayTimeEnglish()
