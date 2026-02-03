8bcf22766115ac4bb4084b660ddb66073af6077b
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-02-03 13:44:19 -0700

    Messed with fizzbuzz alternatives

--- practices/Fizzbuzz.py
+++ practices/Fizzbuzz.py
@@ -10,3 +10,15 @@ for i in range(1,51):
         print("Buzz")
     else: 
         print(i)
+"""statement = ""
+for i in range(1,51):
+    if i%3 == 0 or i%5 == 0:
+        if i%3 == 0:
+            statement + "Fizz"
+        if i%5 == 0:
+            statement + "Buzz"
+    else:
+        statement = i
+    print(statement)
+    statement = ""
+"""""
