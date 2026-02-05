132de0278d35ffd808cccbaa18c0acf76225262c
Sidus-Lupus <cannon.warren@ucas-edu.net>
2026-02-05 13:32:27 -0700

    Completed functions notes and updated_finacial_calculator

---
+++ notes/functions.py
@@ -0,0 +1,32 @@
+#CW functions
+
+def hello(name):
+    print(f"Hello {name}!")
+
+def full_name(first_name, last_name):
+    return f"{first_name} {last_name}"
+print(f"Hello {full_name("Cannon", "Warren")}")
+person_one = full_name("Lupus", "Sidereus")
+print(f"The second person is {person_one}")
+person = full_name("Remington", "Warren")
+hello(person)
+hello("Purple platapus")
+
+def factorial(number):
+    total = 1
+    for x in range(number, 1, -1):
+        total *= x
+    return total
+
+for y in range(1,10):
+    print(f"The factorial of {y} is {factorial(y)}")
+
+num = 1
+
+def add():
+    num = 3
+    num += 1
+    return num
+
+print(add())
+print(num)
--- practices/updated_financial_calculator.py
+++ practices/updated_financial_calculator.py
@@ -1,16 +1,23 @@
 #CW Financial Calculator
 
-user_income = float(input("What is your monthly income: "))
-user_rent = float(input("What is your monthly rent/mortage: "))
-user_utilities = float(input("What is your montly utitilites bill: "))
-user_groceries = float(input("What is your monthly groceries cost: "))
-user_transport = float(input("What is your monthly transportaiton fees: "))
+def callUserPrice(type):
+    user_price = float(input(f"What is your monthly {type}: "))
+    return user_price
 
+user_income = callUserPrice("income")
+user_rent = callUserPrice("rent/mortage")
+user_utilities = callUserPrice("utitilites")
+user_groceries = callUserPrice("groceries")
+user_transport = callUserPrice("transportaiton")
 
-rent_percent = user_rent/user_income
-uitilites_percent = user_utilities/user_income
-groceries_percent = user_groceries/user_income
-trasnport_percent = user_transport/user_income
+def calculateIncomePercent(expense):
+    user_percent = expense/user_income
+    return user_percent
+
+rent_percent = calculateIncomePercent(user_rent)
+uitilites_percent = calculateIncomePercent(user_utilities)
+groceries_percent = calculateIncomePercent(user_groceries)
+trasnport_percent = calculateIncomePercent(user_transport)
 
 save_amount = user_income * 0.1
 user_spend = user_income-(user_rent + user_utilities + user_groceries + user_transport + (save_amount))
