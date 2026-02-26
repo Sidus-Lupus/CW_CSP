1eb793dad97a13a955960d03ec612c74690edb40
sidus lupus <cannon.warren@ucas-edu.net>
2026-02-26 13:43:41 -0700

    continued time of day

--- C/practices/time_of_day.c
+++ C/practices/time_of_day.c
@@ -3,8 +3,15 @@
 
 int main(){
     int current_time = 0;
-    
-    
+
+    printf("What is the current time of day (24 hour time. E.g. 5:30 pm is 1730): ");
+    scanf("%d", current_time);
+
+    if(current_time >= 600 && current_time < 1200){
+        printf("Good morning user");
+    } else if(current_time >= 1200 && current_time < 1800){
+        printf("Good afternoon user");
+    }
 
     return 0;
 }
